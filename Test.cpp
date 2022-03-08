db5886c46dc646eb61c07e7d0d0d8d41a81f432d
roee-tal <roeetal979@gmail.com>
2022-03-08 19:07:45 +0200

    final

--- Test.cpp
+++ Test.cpp
@@ -14,19 +14,102 @@ string nospaces(string input) {
 	return input;
 }
 
+//Check identity of the function
 TEST_CASE("good input")
 {
-    CHECK(nospaces(ariel::mat(9, 7, '@', '-')) == nospaces("@@@@@@@@@\n@-------@\n@-@@@@@-@\n@-@---@-@\n@-@@@@@-@\n@-------@\n@@@@@@@@@"));
+    CHECK(nospaces(mat(9, 7, '@', '-')) == nospaces("@@@@@@@@@\n"
+													"@-------@\n"
+													"@-@@@@@-@\n"
+													"@-@---@-@\n"
+													"@-@@@@@-@\n"
+													"@-------@\n"
+													"@@@@@@@@@"));
+
     CHECK(nospaces(ariel::mat(1, 1, '!', '*')) == nospaces("!"));
-    CHECK(nospaces(ariel::mat(1, 7, '@', '-')) == nospaces("@@@@@@@@@"));
-    CHECK(nospaces(ariel::mat(7, 1, '^', '(')) == nospaces("^\n^\n^\n^\n^\n^\n^\n"));
-    CHECK(nospaces(ariel::mat(11, 3, '*', ')')) == nospaces("***********\n*)))))))))*\n***********\n"));
-    CHECK(nospaces(ariel::mat(5, 3, '$', '&')) == nospaces("$$$$$\n$&&&$\n$$$$$"));
-    CHECK(nospaces(ariel::mat(3, 3, '%', ' ')) == nospaces("%%%%%%%%"));
+
+    CHECK(nospaces(ariel::mat(7, 1, '@', '-')) == nospaces("@@@@@@@@@"));
+
+    CHECK(nospaces(ariel::mat(1, 7, '^', '(')) == nospaces("^\n^\n^\n^\n^\n^\n^\n"));\
+
+    CHECK(nospaces(ariel::mat(11, 3, '*', ')')) == nospaces("***********\n"
+                                                            "*)))))))))*\n"
+                                                            "***********"));
+
+    CHECK(nospaces(ariel::mat(5, 3, '$', '&')) == nospaces("$$$$$\n"
+                                                           "$&&&$\n"
+                                                           "$$$$$"));
+
+    CHECK(nospaces(ariel::mat(3, 3, '%', ' ')) == nospaces("%%%\n"
+                                                            "%%\n"
+                                                            "%%%"));
+    //Check same sign
+    CHECK(nospaces(ariel::mat(5, 3, '%', '%')) == nospaces("%%%%%\n"
+                                                            "%%%%%\n"
+                                                            "%%%%%"));                                                        
+
     CHECK(nospaces(ariel::mat(13, 9, '#', '!')) == nospaces("#############\n#!!!!!!!!!!!#\n#!#########!#\n#!#!!!!!!!#!#\n#!#!#####!#!#\n#!#!!!!!!!#!#\n#!#########!#\n#!!!!!!!!!!!#\n#############\n"));
-                                                          
 }
 
+TEST_CASE("Check first and last row"){
+
+    string res = ariel::mat(5,3,'@','*');
+    string first_row;
+    for(int i=0;i<5;i++){
+        first_row.push_back(res[i]);
+    }    
+    string last_row;
+    int end = res.length();
+    for(int i=end-2;i>end-7;i--){
+        last_row.push_back(res[i]);
+    }    
+    // The first and last row supposed to by the same
+    CHECK(nospaces(first_row) == nospaces(last_row)); 
+
+    // Check now for more rows than cols
+    string res2 = ariel::mat(1,7,'^','(');
+    string first_row2;
+    for(int i=0;i<1;i++){
+        first_row2.push_back(res[i]);
+    }    
+    string last_row2;
+    int end2 = res.length();
+    for(int i=end2-2;i>end2-3;i--){
+        last_row2.push_back(res[i]);
+    }    
+    CHECK(nospaces(first_row2) == nospaces(last_row2));                                                                                                                                      
+}
+
+// The output should be a palindrome
+TEST_CASE("palindrome"){
+    string ans;
+    for(int i=1, j=3; i<11; i=i+2, j=j+2){
+        ans = nospaces(ariel::mat(j,i,'#','-'));
+        int len = ans.length();
+        for(int k=0;k<len;k++){
+            if(ans[k] != ans[len-k-1]){ //If the chars are different the input is not a palindrome
+                FAIL_CHECK("False");
+                break;
+            }
+        }
+        cout << "Success\n";
+    }
+
+    // Check now for more rows
+    string ans2;
+    for(int i=3, j=1; j<11; i=i+2, j=j+2){
+        ans = nospaces(ariel::mat(j,i,'#','-'));
+        int len = ans.length();
+        for(int k=0;k<len;k++){
+            if(ans[k] != ans[len-k-1]){
+                FAIL_CHECK("False");
+                break;
+            }
+        }
+        cout << "Success\n";
+    }
+}
+
+//This should throw exception
 TEST_CASE("bad input")
 {
     CHECK_THROWS(ariel::mat(4,0,'#','$'));
@@ -38,5 +121,12 @@ TEST_CASE("bad input")
     CHECK_THROWS(ariel::mat(-1,3,'#','*'));
     CHECK_THROWS(ariel::mat(3,-1,'#','*'));
     CHECK_THROWS(ariel::mat(-1,-1,'#','*'));
+    CHECK_THROWS(ariel::mat(-1,-1,'#','*'));
+
+
+
+
+
+
 
 }
--- mat.cpp
+++ mat.cpp
@@ -5,27 +5,22 @@ using namespace std;
 
 namespace ariel {
     string mat(int len, int height, char sign1, char sign2){
-        if(len %2 == 0 || height %2 == 0){
+        if(len % 2 == 0 || height % 2 == 0){
             throw invalid_argument("Mat size is always odd");
         }
         if(len <0 || height < 0)
         {
             throw invalid_argument("Mat size is always positive");
         }
-        char matrix[height][len];  
-        for(int i=0;i<height;i++){
-            for(int j=0;j<len;j++){
-                matrix[i][j] = sign1;
-            }
-
-        }
         string res;
-         for(int i=0;i<height;i++){
-            for(int j=0;j<len;j++){
-                res.push_back(matrix[i][j]);
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < len; j++)
+            {
+                res.push_back(sign1);
             }
-            res += "\n";
+            res.push_back('\n');
+        }
+        return res;
     }
-    return res;
-}
 }
